2d347cbd286b265f977069861ffc763b029cf6a4
Xing Tao Shi <xshi@stuy.edu>
2020-01-07 14:18:51 -0500

    first assignment done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+//App.js
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import PropTypes from 'prop-types'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ContactCard extends Component {
+    constructor(props) {
+	super(props);
+    }
+    render() {
+	return <div><h4>{this.props.username}</h4>{this.props.mobile}<br/>{this.props.cell}<br/>{this.props.email}</div>;	    
+    }
+}
+
+ContactCard.propTypes = {
+    username: PropTypes.string.isRequired,
+    mobile: PropTypes.string.isRequired,
+    cell: PropTypes.string.isRequired,
+    email: PropTypes.string
+}
+
+ContactCard.defaultProps = {
+    username: "1",
+    mobile: "2",
+    cell: "3",
+    email: "4"
+}
+
+
+class App extends Component {
+    render() {	
+	return (<div class="App-header"> 
+		<ContactCard username="Dwayne Johnson" mobile="(123)-456-7890" cell="(123)-456-7290" email="rocky@gmail.com"/><br/>
+		<ContactCard username="Friend Jack" mobile="(173)-466-1290" cell="(123)-123-7893" email="friend@gmail.com"/><br/>
+		<ContactCard username="Ethan Chen" mobile="(163)-453-7123" cell="(123)-333-7450" email="ec23@gmail.com"/><br/>
+		</div>
+		);
+	
+    }
 }
 
 export default App;
